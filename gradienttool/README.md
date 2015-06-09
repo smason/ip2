ee68acfad40d43cc64e590e1ac11817df99b9071
Sam Mason <sam.mason@warwick.ac.uk>
2015-06-09 16:39:26 +0100

    more fiddling with gradient tool

---
+++ gradienttool/README.md
@@ -0,0 +1,10 @@
+# Gradient Tool #
+
+This is the Python version of the "Gradient Tool" from Warwick's
+Systems Biology Centre.
+
+
+## Application Notes ##
+
+It is assumed that testing is *destructive*, such that variance within
+the data of each replicate is the same as that between replicates.
--- gradienttool/gradienttool.py
+++ gradienttool/gradienttool.py
@@ -1,29 +1,67 @@
 from __future__ import absolute_import, division, print_function
 
-import sys
-import csv
+import numpy       as np
+import scipy.stats as sps
 
-import numpy as np
-import scipy as sp
+def squaredDistance(x):
+    "Calculate squared-distance between every pair of elements in the given vector"
+    return (x - x[:,None]) ** 2
 
-numreps = 4
+def covSquaredExponential(x, ell, sf2):
+    "Squared Exponential covariance function, as per covSEiso from GPML."
+    return sf2 * np.exp(-squaredDistance(x / ell) / 2)
 
-inp = csv.reader(sys.stdin)
+def covNoise(x, s2):
+    "Independent covariance function, as per covNoise from GPML."
+    return s2 * np.eye(len(x))
 
-hdr = next(inp)
-names = hdr[1:]
-rows = []
-data = []
+class GaussianProcess:
+    def __init__(self, cov):
+        self.cov = cov
 
-print(names)
+class NamedMatrix:
+    def __init__(self, colnames, rownames, data):
+        assert data.shape == (len(rownames),len(colnames))
+        self.colnames = colnames
+        self.rownames = rownames
+        self.data     = data
 
-for l in inp:
-    rows.append(l[0])
-    data.append(l[1:])
+def readNamedMatrix(itr, dtype=np.float64):
+    "Read a matrix that has a single row&column of names."
+    hdr = next(itr)
+    names = hdr[1:]
+    rows = []
+    data = []
 
-data = np.asarray(data, dtype=np.float64)
-data.shape = (data.shape[0],numreps,data.shape[1]//numreps)
+    for l in itr:
+        rows.append(l[0])
+        data.append(l[1:])
 
-print(data)
+    return NamedMatrix(names, rows, np.asarray(data, dtype=dtype))
 
-time = np.tile(np.linspace(0, 1, numtimepoints),numreps)
+def calcGradients(x, y):
+    def opt(theta):
+        # parameters are constrained to be > 0
+        theta = np.exp(theta)
+        cov = (
+            covSquaredExponential(x, theta[0], theta[1]) +
+            covNoise(x, theta[2]))
+        ll = (
+            sps.multivariate_normal.logpdf(y, mean=None, cov=cov) +
+            sps.gamma.logpdf(theta, 2))
+
+if __name__ == "__main__":
+    import sys
+    import csv
+    import re
+
+    mat = readNamedMatrix(csv.reader(sys.stdin))
+
+    # remove the "TP" that has been prepended to the times
+    time = [re.sub("^TP","",x) for x in mat.colnames]
+
+    # convert colnames into a NumPy matrix
+    time = np.asarray(time, dtype=np.float64)
+
+    print(mat.data.shape)
+    print(time.shape)
