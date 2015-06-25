d4cff74065ec80742060ef45ecff6aeb925639ac
Sam Mason <sam.mason@warwick.ac.uk>
2015-06-25 16:35:32 +0100

    move main() into its own __main__.py file inside gradienttool

---
+++ gradienttool/__main__.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import, division, print_function
+
+import csv
+import re
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+import seaborn as sbs
+
+from gradienttool import GradientTool
+
+def normaliseArray(x):
+    mn = np.nanmin(x)
+    mx = np.nanmax(x)
+    return (x - mn) / (mx - mn)
+
+def main():
+    plt.switch_backend("pdf")
+
+    import shared.filehandling as sfh
+
+    inp = sfh.readCsvNamedMatrix(open("gradienttool/testdata/demData.csv"))
+
+    # assume column headers are time, so remove anything that isn't
+    # part of a number
+    t = [re.sub("[^0-9.]","",x) for x in inp.colnames]
+
+    # convert colnames into a NumPy matrix and normalise
+    t = normaliseArray(np.asarray(t, dtype=np.float64))
+
+    with open("output.csv",'w') as csvfd:
+        with PdfPages("output.pdf") as pdf:
+            csvout = csv.writer(csvfd)
+            csvout.writerow(["item","X","f_mean","f_variance","df_mean","df_variance","tscore"])
+
+            for i in range(inp.nrows()):
+                name = inp.rownames[i]
+
+                gt = GradientTool(t, inp.data[i,:])
+                gt.setPriorRbfLengthscale(2.0, 0.2)
+                gt.setPriorRbfVariance(2.0, 0.5)
+                gt.setPriorNoiseVariance(1.5, 0.1)
+
+                gt.optimize()
+                fig = plt.figure(figsize=(8,7))
+                gt.plot(name,fig)
+                pdf.savefig()
+                plt.close(fig)
+
+                for r in gt.getResults():
+                    csvout.writerow([name]+r.tolist())
+
+if __name__ == "__main__":
+    main()
--- gradienttool/gradienttool.py
+++ gradienttool/gradienttool.py
@@ -8,11 +8,6 @@ import matplotlib.pyplot as plt
 import GPy
 import GPy.plotting.matplot_dep.Tango as Tango
 
-def normaliseArray(x):
-    mn = np.nanmin(x)
-    mx = np.nanmax(x)
-    return (x - mn) / (mx - mn)
-
 class GradientTool:
     """Code for running WSBC's Gradient-Tool analysis
 
@@ -86,48 +81,3 @@ class GradientTool:
             ax2.axhline(y,lw=1,ls='--',c='black')
         ax2.vlines(res[:,0], [0], res[:,5]);
         # figure.tight_layout()
-
-def _main():
-    import csv
-    import re
-
-    import seaborn as sbs
-    from matplotlib.backends.backend_pdf import PdfPages
-
-    plt.switch_backend("pdf")
-
-    import shared.filehandling as sfh
-
-    inp = sfh.readCsvNamedMatrix(open("gradienttool/testdata/demData.csv"))
-
-    # assume column headers are time, so remove anything that isn't
-    # part of a number
-    t = [re.sub("[^0-9.]","",x) for x in inp.colnames]
-
-    # convert colnames into a NumPy matrix and normalise
-    t = normaliseArray(np.asarray(t, dtype=np.float64))
-
-    with open("output.csv",'w') as csvfd:
-        with PdfPages("output.pdf") as pdf:
-            csvout = csv.writer(csvfd)
-            csvout.writerow(["item","X","f_mean","f_variance","df_mean","df_variance","tscore"])
-
-            for i in range(inp.nrows()):
-                name = inp.rownames[i]
-
-                gt = GradientTool(t, inp.data[i,:])
-                gt.setPriorRbfLengthscale(2.0, 0.2)
-                gt.setPriorRbfVariance(2.0, 0.5)
-                gt.setPriorNoiseVariance(1.5, 0.1)
-
-                gt.optimize()
-                fig = plt.figure(figsize=(8,7))
-                gt.plot(name,fig)
-                pdf.savefig()
-                plt.close(fig)
-
-                for r in gt.getResults():
-                    csvout.writerow([name]+r.tolist())
-
-if __name__ == "__main__":
-    _main()
