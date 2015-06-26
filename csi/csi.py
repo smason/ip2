0911348a210e8b545365f38c60a3442f58c9489d
Sam Mason <sam@samason.uk>
2015-06-26 10:08:12 +0100

    starting work on CSI

--- csi/csi.py
+++ csi/csi.py
@@ -1,8 +1,23 @@
 import numpy as np
 import scipy as sp
-import GPy
+# import GPy
 
-import shared.filehandling as sfh
+import itertools as it
+
+def parentalSets(items, item, depth):
+    """Iterate over all "Parental Sets".
+
+    A parental set is a list of potential regulators/transcription
+    factors of a gene.
+    """
+
+    l = list(items)
+    l.remove(item)
+
+    yield (item,)
+    for i in range(depth):
+        for m in it.combinations(l, 1+i):
+            yield (item,)+m
 
 # The whole of CSI_Engine should basically move into GPy.  It's going
 # to be a bit of a fiddle figuring out how to set everything up, but
@@ -14,7 +29,9 @@ if __name__ == "__main__":
     import sys
     import csv
 
+    import shared.filehandling as sfh
+
     with open("csi/testdata/Demo_DREAM.csv") as fd:
         data = sfh.readCsvNamedMatrix(fd)
 
-    print data
+    print(data)
