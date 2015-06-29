f882908aff7924057ea5c83fc564e81da151a2cb
Sam Mason <sam.mason@warwick.ac.uk>
2015-06-29 12:30:02 +0100

    improved docs in CSI

--- csi/csi.py
+++ csi/csi.py
@@ -1,23 +1,38 @@
 import numpy as np
 import scipy as sp
-# import GPy
+import GPy
 
 import itertools as it
+
+class MetaModel(GPy.core.Model):
+    def __init__(self, models):
+        #models is a  list of GPy models.
+        GPy.core.Model.__init__(self, name='metamodel')
+        self.models = models
+        self.link_parameters(*self.models)
+
+    def log_likelihood(self):
+        return sum([m.log_likelihood() for m in self.models])
 
 def parentalSets(items, item, depth):
     """Iterate over all "Parental Sets".
 
-    A parental set is a list of potential regulators/transcription
-    factors of a gene.
+    A parental set is a list of regulators/transcription factors for a
+    specific gene.  The parental sets for any given item is every
+    subset of items up to a given depth that does not include the
+    item.
     """
 
+    # exclude the target, duplicate list to avoid modifying callers
+    # state
     l = list(items)
     l.remove(item)
 
-    yield (item,)
-    for i in range(depth):
-        for m in it.combinations(l, 1+i):
-            yield (item,)+m
+    for i in range(0, depth+1):
+        # iterate over every subset of size i
+        for subset in it.combinations(l, i):
+            yield (item,list(subset))
+
 
 # The whole of CSI_Engine should basically move into GPy.  It's going
 # to be a bit of a fiddle figuring out how to set everything up, but
