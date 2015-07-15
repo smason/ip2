22d6ead154c0395a26cb96ef6255a51d55b352ba
Sam Mason <sam@samason.uk>
2015-07-15 11:42:28 +0100

    gradient tool tidy up

--- gradienttool/__main__.py
+++ gradienttool/__main__.py
@@ -14,63 +14,6 @@ import seaborn as sbs
 import shared as ip2
 import gradienttool as gt
 
-def normaliseArray(x):
-    """linearly scale a NumPy array @x such that the minimum is zero and the maximum is one."""
-    mn = np.nanmin(x)
-    mx = np.nanmax(x)
-    return (x - mn) / (mx - mn)
-
-def doReportPlot(fig, g, xtrans, ytrans, title=None,
-                 CI=sp.stats.norm.ppf([0.025,0.975])):
-    """Generate a pair of plots for the report"""
-    res1 = g.getResults()
-    ar = ip2.niceAxisRange(res1.index)
-    res2 = g.getResults(np.linspace(ar[0],ar[1],101))
-
-    # transform back to unnormalised space
-    res1 = gt.transformResults(res1, xtrans, ytrans)
-    res2 = gt.transformResults(res2, xtrans, ytrans)
-
-    # extract for ease of access
-    mu     = res2['mu']
-    var    = res2['var']
-    mud    = res2['mud']
-    tscore = res1['tscore']
-
-    # set up plots
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212, sharex=ax1)
-
-    ax1.margins(0.04)
-    ax2.set_ylim([-6,6])
-
-    fig.tight_layout()
-
-    if title is not None: ax1.set_title(title)
-
-    # draw data, function and error bars
-    ax1.fill_between(mu.index,
-                     mu + CI[0]*np.sqrt(var + g.noiseVariance),
-                     mu + CI[1]*np.sqrt(var + g.noiseVariance),
-                     facecolor='black', edgecolor='none', alpha=0.05)
-    ax1.fill_between(mu.index,
-                     mu + CI[0]*np.sqrt(var),
-                     mu + CI[1]*np.sqrt(var),
-                     facecolor='royalblue', edgecolor='none', alpha=0.2)
-    mu.plot(ax=ax1)
-    # data goes at the end so it doesn't get hidden by everything else
-    ax1.scatter(g.X * xtrans[1] + xtrans[0],
-                g.Y * ytrans[1] + ytrans[0],
-                marker='x', c='black', s=40, lw=1.2);
-
-    # draw t-scores
-    ax2.vlines(tscore.index, 0, tscore)
-    ax2.plot(res2.index, res2['tscore'])
-    for y in CI:
-        ax2.axhline(y, lw=1, ls='--', color='black')
-
-    return res1
-
 def main():
     plt.switch_backend('pdf')
 
@@ -86,16 +29,15 @@ def main():
     xtrans = (min(time), max(time)-min(time))
     ytrans = (np.mean(inp.data), np.std(inp.data))
 
-    time = (time - xtrans[0]) / xtrans[1]
-
     gl = []
+    zeros = {}
     with open('output.csv','w') as csvfd:
         csvout = csv.writer(csvfd)
         csvout.writerow(['item','X','f_mean','f_variance','df_mean','df_variance','tscore'])
 
         for i in range(inp.nrows()):
             # run the gradient tool
-            g = gt.GradientTool(time, (inp.data[i,:] - ytrans[0]) / ytrans[1])
+            g = gt.GradientToolNormalised(time, inp.data[i,:], xtrans, ytrans)
             g.setPriorRbfLengthscale(2.0, 0.2)
             g.setPriorRbfVariance(2.0, 0.5)
             g.setPriorNoiseVariance(1.5, 0.1)
@@ -104,19 +46,37 @@ def main():
             # save results for plotting
             gl.append(g)
 
+            # get the "results" out
+            res = g.getResults()
+
+            zt = gt.zerosLinearInterpolate(np.vstack([res.index,res["mud"]]).T)
+            if len(zt) in zeros:
+                zeros[len(zt)] = np.concatenate([zeros[len(zt)], zt])
+            else:
+                zeros[len(zt)] = zt
+
             # write out to CSV file
             name = inp.rownames[i]
-            res = gt.transformResults(g.getResults(), xtrans, ytrans)
             for i,k in res.iterrows():
                 csvout.writerow([name,i]+k.tolist())
 
     # write out a nicely ordered PDF file
     with PdfPages('output.pdf') as pdf:
+        for a,b in zeros.items():
+            fig = plt.figure(figsize=(6,4))
+            ax1 = fig.add_subplot(111)
+            ax1.margins(0.04)
+            ax1.hist(b, edgecolor='none')
+            ax1.set_title("%i Gradient Inflections" % (a,))
+            pdf.savefig()
+            plt.close(fig)
+
         for i in np.argsort([-g.rbfLengthscale for g in gl]):
             g = gl[i]
 
             fig = plt.figure(figsize=(8,7))
-            res = doReportPlot(fig, g, xtrans, ytrans, title=inp.rownames[i])
+            gt.doReportPlot(fig, g, title=inp.rownames[i])
+            fig.tight_layout()
             pdf.savefig()
             plt.close(fig)
 
--- gradienttool/gradienttool.py
+++ gradienttool/gradienttool.py
@@ -23,20 +23,50 @@ def zerosLinearInterpolate(xy):
         out[j] = -c/m
     return out
 
-def transformResults(res, xtrans, ytrans):
-    """Linearly transform results"""
-    out = res.set_index(res.index * xtrans[1] + xtrans[0])
-
-    out["mu"]   = out["mu"] * ytrans[1] + ytrans[0]
-    out["var"]  = out["var"] * ytrans[1] ** 2
-    out["mud"]  = out["mud"] * (ytrans[1]/xtrans[1])
-    out["vard"] = out["vard"] * (ytrans[1]/xtrans[1])**2
-
-    return out
-
 def _test():
     assert np.allclose(0.5,zerosLinearInterpolate(np.array([[0,-0.5],
                                                             [1, 0.5]])))
+
+def doReportPlot(fig, g, title=None,
+                 CI=sp.stats.norm.ppf([0.025,0.975])):
+    """Generate a pair of plots for the report"""
+    res1 = g.getResults()
+    ar = ip2.niceAxisRange(res1.index)
+    res2 = g.getResults(np.linspace(ar[0],ar[1],101))
+
+    # extract for ease of access
+    mu     = res2['mu']
+    var    = res2['var']
+    mud    = res2['mud']
+    tscore = res1['tscore']
+
+    # set up plots
+    ax1 = fig.add_subplot(211)
+    ax2 = fig.add_subplot(212, sharex=ax1)
+
+    if title is not None: ax1.set_title(title)
+
+    ax1.margins(0.04)
+    ax2.set_ylim([-6,6])
+
+    # draw data, function and error bars
+    ax1.fill_between(mu.index,
+                     mu + CI[0]*np.sqrt(var + g.noiseVariance),
+                     mu + CI[1]*np.sqrt(var + g.noiseVariance),
+                     facecolor='black', edgecolor='none', alpha=0.05)
+    ax1.fill_between(mu.index,
+                     mu + CI[0]*np.sqrt(var),
+                     mu + CI[1]*np.sqrt(var),
+                     facecolor='royalblue', edgecolor='none', alpha=0.2)
+    mu.plot(ax=ax1)
+    # data goes at the end so it doesn't get hidden by everything else
+    ax1.scatter(g.X, g.Y, marker='x', c='black', s=40, lw=1.2);
+
+    # draw t-scores
+    ax2.vlines(tscore.index, 0, tscore)
+    ax2.plot(res2.index, res2['tscore'])
+    for y in CI:
+        ax2.axhline(y, lw=1, ls='--', color='black')
 
 class GradientTool:
     """Code for running WSBC's Gradient-Tool analysis
@@ -48,9 +78,14 @@ class GradientTool:
     def __init__(self, X, Y):
         assert len(X.shape) == 1
         assert X.shape == Y.shape
-        self.X = X
-        self.Y = Y
+        self._X = X
+        self._Y = Y
         self.m = GPy.models.GPRegression(X[:,None], Y[:,None])
+
+    @property
+    def X(self): return self._X
+    @property
+    def Y(self): return self._Y
 
     @property
     def rbfLengthscale(self): return float(self.m.rbf.lengthscale)
@@ -76,7 +111,7 @@ class GradientTool:
 
         # get the points we're evaluating the function at
         if Xstar is None:
-            Xstar = np.unique(self.X)
+            Xstar = np.unique(self._X)
 
         # get predictions of our function and its derivative
         mu,var = self.m._raw_predict(Xstar[:,None])
@@ -93,3 +128,42 @@ class GradientTool:
         # convert to a DataFrame and return
         return pd.DataFrame(out, index=Xstar, columns=
                             ["mu","var","mud","vard","tscore"])
+
+class GradientToolNormalised(GradientTool):
+    """Wrap the basic GradientTool up such that X and Y are both """
+    def __init__(self, X, Y, xtrans=None, ytrans=None):
+        if xtrans is not None: xtrans = (min(X), max(X)-min(X))
+        if ytrans is not None: ytrans = (np.mean(Y), np.std(Y))
+
+        self.tx = xtrans
+        self.ty = ytrans
+
+        super(GradientToolNormalised, self).__init__((X - self.tx[0]) / self.tx[1],
+                                                     (Y - self.ty[0]) / self.ty[1])
+
+    @property
+    def X(self): return self._X * self.tx[1] + self.tx[0]
+    @property
+    def Y(self): return self._Y * self.ty[1] + self.ty[0]
+
+    @property
+    def rbfLengthscale(self): return float(self.m.rbf.lengthscale) * self.tx[1]**2
+    @property
+    def rbfVariance   (self): return float(self.m.rbf.variance) * self.ty[1]**2
+    @property
+    def noiseVariance (self): return float(self.m.Gaussian_noise.variance) * self.ty[1]**2
+
+    def getResults(self, Xstar=None):
+        if Xstar is not None:
+            Xstar = (Xstar - self.tx[0]) / self.tx[1]
+
+        res = super(GradientToolNormalised, self).getResults(Xstar)
+
+        res.set_index(res.index * self.tx[1] + self.tx[0], inplace=True)
+
+        res["mu"]   = res["mu"] * self.ty[1] + self.ty[0]
+        res["var"]  = res["var"] * self.ty[1] ** 2
+        res["mud"]  = res["mud"] * (self.ty[1]/self.tx[1])
+        res["vard"] = res["vard"] * (self.ty[1]/self.tx[1])**2
+
+        return res
