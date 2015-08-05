d89821f401f671a7bbf027dd8da3cc379605c3a2
Sam Mason <sam@samason.uk>
2015-08-05 10:38:53 +0100

    cleaning up main CSI code

--- csi/__main__.py
+++ csi/__main__.py
@@ -18,60 +18,75 @@ logger = logging.getLogger('CSI')
 def cmdparser(args):
     op = optparse.OptionParser()
     op.set_usage("usage: %prog [options] FILE.csv")
-    op.set_defaults(verbose=False,normalise=True)
+    op.set_defaults(
+        verbose=False,
+        normalise=True,
+        depth=2,
+        depgenes=[])
     op.add_option('-v','--verbose',dest='verbose',action='count',
                   help="Increase verbosity (specify twice for more detail)")
     op.add_option('-o','--csvoutput',dest='csvoutput',
                   help="write CSV output to FILE", metavar='FILE')
     op.add_option('-p','--pdfoutput',dest='pdfoutput',
                   help="write PDF output to FILE", metavar='FILE')
+    op.add_option('-d', '--depth',dest='depth',type='int',action='store',
+                  help="Truncation depth for parental set")
+    op.add_option('--gene',dest='genes',action='append',
+                  help="Specific gene to analyse (specify again for more than one)")
     return op.parse_args(args)
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
 
+    # parse command line arguments
     (op,fname) = cmdparser(args)
 
+    # extract out the logging level early
     log_level = logging.WARNING
-    if op.verbose == 1:
-        log_level = logging.INFO
-    elif op.verbose >= 2:
-        log_level = logging.DEBUG
+    if   op.verbose == 1: log_level = logging.INFO
+    elif op.verbose >= 2: log_level = logging.DEBUG
 
+    # configure logger
     logging.basicConfig(level=log_level)
     logging.getLogger('GP').setLevel(logging.WARNING)
     logging.getLogger('parameters changed meta').setLevel(logging.WARNING)
 
-    if len(fname) == 0:
-        sys.stderr.write("Error: Please specify the filename to process, or run with '-h' for more options\n")
-        sys.exit(1)
-
+    # make sure we're only processing a single file
     if len(fname) != 1:
-        sys.stderr.write("Error: Only one input filename currently supported\n")
+        if len(fname) == 0:
+            sys.stderr.write("Error: Please specify the filename to process, or run with '-h' for more options\n")
+        else:
+            sys.stderr.write("Error: Only one input filename currently supported\n")
         sys.exit(1)
 
-    if op.csvoutput is None:
+    # pull out the parental set trunction depth and validate
+    depth = op.depth
+    if depth < 1:
+        sys.stderr.write("Error: truncation depth must be greater than or equal to one")
+        sys.exit(1)
+
+    # sanity check!
+    if depth == 1:
+        logger.info("Truncation depth of 1 may not be very useful")
+
+    # figure out where our output is going
+    if op.csvoutput is None or op.csvoutput == '-':
         csvout = csv.writer(sys.stdout)
     else:
         csvout = csv.writer(open(op.csvoutput,'w'))
 
+    # read in data and make sure headers are correct
     inp = pd.read_csv(open(fname[0]),dtype=str,index_col=0,header=[0,1])
     inp.columns = pd.MultiIndex.from_tuples([(a,float(b)) for a,b in inp.columns],
                                             names=inp.columns.names)
     # convert to floating point values
     inp = inp.astype(float)
 
-    # check whether we have two column headers and that the second
-    # level is sorted (currently check whether all levels are sorted,
-    # need to fix!)
-    assert (isinstance(inp.columns,pd.MultiIndex) and
-            len(inp.columns.levels) == 2 and
-            inp.columns.is_monotonic_increasing)
+    # check whether the second level is sorted (currently check whether all
+    # levels are sorted, need to fix!)
+    assert (inp.columns.is_monotonic_increasing)
     # not sure whether I can do anything similar for the rows
-
-    # TODO: get this from the command line
-    depth = 2
 
     if op.verbose:
         logger.info("Genes: %s",
@@ -81,12 +96,17 @@ def main(args=None):
         logger.info("Time: %s",
                     ", ".join([repr(x) for x in inp.columns.levels[1]]))
 
+    genes = op.genes
+    if genes is None:
+        logger.debug("No genes specified, assuming all")
+        genes = list(inp.index)
+
     # start the CSI analysis
     cc = csi.Csi(inp)
     # we only know how to do expectation-maximisation at the moment
     em = cc.getEm()
 
-    for gene in inp.index:
+    for gene in genes:
         logger.info("Processing: %s", repr(gene))
 
         em.setup(cc.allParents(gene,depth))
@@ -102,9 +122,12 @@ def main(args=None):
 
         logger.debug("finished after %i iterations", ittr)
 
-        csvout.writerow([
-            ])
-
+        ll = em.logliks()
+        for i in np.argsort(-ll):
+            csvout.writerow(
+                [gene,em.weights[i]]+
+                em.hypers.tolist()+
+                [":".join(em.pset[i][0])])
 
 if __name__ == '__main__':
     main()
