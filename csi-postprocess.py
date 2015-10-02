223eddad9f619084ed0052d420285fa022483726
Sam Mason <sam.mason@warwick.ac.uk>
2015-10-02 15:19:19 +0100

    CSI postprocessing now handles command line processing

--- csi-postprocess.py
+++ csi-postprocess.py
@@ -4,6 +4,9 @@ import sys
 import h5py as h5
 import csi.h5reader as pp
 
+import optparse
+import logging
+
 def get_dom(mod, min_weight=1e-5, min_predict=1e-2):
     items = mod.items
 
@@ -44,10 +47,86 @@ def get_dom(mod, min_weight=1e-5, min_predict=1e-2):
         reps=reps,
         results=results)
 
-def main():
-    fd = h5.File("dream-trunc3.h5")
-    mod = pp.Model(fd)
-    json.dump(get_dom(mod),sys.stdout)
+def merge_dom(mod1, mod2):
+    if mod1["items"] != mod2["items"]:
+        raise ValueError("Items not the same")
+    if mod1["reps"] != mod2["reps"]:
+        raise ValueError("Data/replicates not the same")
+    return dict(
+        items=mod1["items"],
+        reps=mod1["reps"],
+        results=mod1["results"]+mod2["results"])
+
+def cmdparser(args):
+    # create parser objects
+    op = optparse.OptionParser()
+    out = optparse.OptionGroup(op, "Output Formats")
+
+    op.set_usage("usage: %prog [options] FILE1.hdf5 [FILE2.hdf5]")
+    op.set_defaults(
+        verbose=False,
+        min_weight=1e-5)
+
+    # define general parameters
+    op.add_option('-v','--verbose',dest='verbose',action='count',
+                  help="Increase verbosity (specify twice for more detail)")
+
+    op.add_option('-w','--weight',dest='min_weight',type=float,
+                  help="Only extract parental with at least this weight",
+                  metavar="MIN")
+    op.add_option('-p','--predict',dest='min_predict',type=float,
+                  help="Only calculate model predictions from parental with least this weight",
+                  metavar="MIN")
+
+    # parse our command line arguments
+    return op.parse_args(args)
+
+def main(args = None):
+    if args is None:
+        args = sys.argv[1:]
+
+    # parse command line arguments
+    (op,fname) = cmdparser(args)
+
+    # extract out the logging level early
+    log_level = logging.WARNING
+    if   op.verbose == 1: log_level = logging.INFO
+    elif op.verbose >= 2: log_level = logging.DEBUG
+
+    # configure logger
+    logging.basicConfig(level=log_level)
+
+    min_weight = op.min_weight
+    if min_weight < 0 or min_weight > 1:
+        logging.warn("Minimum parental-set weight should be between 0 and 1")
+
+    if op.min_predict is None:
+        min_predict = 1e-2
+    else:
+        min_predict = op.min_predict
+        if min_predict < 0 or min_predict > 1:
+            logging.warn("Minimum weight for predictions should be between 0 and 1")
+
+        if min_predict < min_weight:
+            logging.warn("Minimum prediction weight should be greater than weight filter")
+
+    if len(fname) < 1:
+        logging.error("I need at least one file to process")
+        sys.exit(1)
+
+    try:
+        with h5.File(fname[0],"r") as fd:
+            dom = get_dom(pp.Model(fd), min_weight, min_predict)
+    except IOError as err:
+        sys.stderr.write("Error opening file {path}: {err}\n".format(path=repr(fname[0]), err=str(err)))
+        sys.exit(1)
+
+    for path in fname[1:]:
+        with h5.File(path,"r") as fd:
+            dom = merge_dom(dom,get_dom(pp.Model(fd), min_weight, min_predict))
+
+    sys.stdout.write("csires = ")
+    json.dump(dom,sys.stdout)
 
 if __name__ == '__main__':
     main()
